7fd57048c21283ea4fe25226a06c6fbbdb8f4275
Gerhard Knapp <gerhard.knapp@ars.de>
2024-03-11 17:16:22 +0100

    add all the texts in translation.ts and add buttons to choose language

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,49 @@
-import React, { useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useLocation} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../assets/logo.png';
 import NavOverlay from "./NavOverlay";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useTranslation} from "react-i18next";
 
 const Navbar: React.FC = () => {
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
     const location = useLocation();
+    const {i18n} = useTranslation();
 
     const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
     const isHomePage = location.pathname === '/' || location.pathname === '/imprint';
     const navLinkClass = isHomePage ? "nav-link text-dark" : "nav-link text-white";
     const togglerIconColor = isHomePage ? "text-dark" : "text-white";
 
+    const changeLanguage = (language: string) => {
+        i18n.changeLanguage(language);
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg fixed-top p-4" style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}>
-            <div className="container-fluid align-items-center align-items-lg-start px-2 d-flex justify-content-between">
+        <nav className="navbar navbar-expand-lg fixed-top p-4" style={{backgroundColor: 'rgba(0, 0, 0, 0)'}}>
+            <div
+                className="container-fluid align-items-center align-items-lg-start px-2 d-flex justify-content-between">
+
                 {isNavCollapsed && (
                     <>
                         <Link to="/" className="navbar-brand">
-                            <img src={logo} alt="Logo" />
+                            <img src={logo} alt="Logo"/>
                         </Link>
-                        <button className={`navbar-toggler ${togglerIconColor}`} type="button" onClick={handleNavCollapse}>
-                            <FontAwesomeIcon icon={faBars} />
+                        <button className={`navbar-toggler ${togglerIconColor}`} type="button"
+                                onClick={handleNavCollapse}>
+                            <FontAwesomeIcon icon={faBars}/>
                         </button>
                     </>
                 )}
-                {!isNavCollapsed && <NavOverlay handleNavCollapse={handleNavCollapse} />}
+                <ul>
+                    <li className="nav-item">
+                        <button className="btn btn-light mx-1" onClick={() => changeLanguage('en')}>EN</button>
+                        <button className="btn btn-dark mx-1" onClick={() => changeLanguage('de')}>DE</button>
+                    </li>
+                </ul>
+                {!isNavCollapsed && <NavOverlay handleNavCollapse={handleNavCollapse}/>}
 
                 <div className="navbar-nav-desktop">
                     <ul className="navbar-nav ms-auto">
@@ -48,7 +63,7 @@ const Navbar: React.FC = () => {
                 </div>
             </div>
         </nav>
-    );
+);
 }
 
 export default Navbar;
